a7ab13a42b80a47c4970b7e2cae09cf885288da3
Srivatsava C.K <srivatsava432@gmail.com>
2016-10-07 16:59:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,12 @@ alert("hi the page got loaded");
 var element=document.getElementById('firstid');
 element.innerHTML="new value";
 var image=document.getElementById('img');
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    image.style.marginLeft=marginLeft+'px';
+}
  image.onclick= function()
  {      
-  image.style.marginLeft='100px';  
+  var interval=setInterval(moveRight,40);
 };
