e9751af2acb719f67b971ed2ac4d6ce4e67e2222
Srivatsava C.K <srivatsava432@gmail.com>
2016-10-07 11:26:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res){
-    var articleName=req.parms.articleName;
+    var articleName=req.params.articleName;
   res.send(createTemplate(webpages[articleName]));
 });
 
